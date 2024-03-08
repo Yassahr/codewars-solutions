4602ea898e42bb459b0adb9214c280fd5b4072d0
“Yassah <yassahr@gmail.com>
2024-03-08 17:50:46 -0500

    new 7kyu solution

---
+++ 7kyu/a-z.js
@@ -0,0 +1,21 @@
+// Given a string indicating a range of letters, return a string which includes all the letters in that range, including the last letter.
+// Note that if the range is given in capital letters, return the string in capitals also!
+
+// Examples
+// "a-z" ➞ "abcdefghijklmnopqrstuvwxyz"
+// "h-o" ➞ "hijklmno"
+// "Q-Z" ➞ "QRSTUVWXYZ"
+// "J-J" ➞ "J"
+// Notes
+// A hyphen will separate the two letters in the string.
+// You don't need to worry about error handling in this kata (i.e. both letters will be the same case and the second letter will not be before the first alphabetically).
+function gimmeTheLetters(sp) {
+    let start = sp.charCodeAt(0);
+    let end = sp.charCodeAt(2);
+    let ans=[]
+    for(let i=start; i<=end; i++){
+       ans.push(String.fromCharCode(i) )
+      
+    }
+    return ans.join('')
+  }
