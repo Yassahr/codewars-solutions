ed250f2c20264cdf61611c3330db037d78f1946e
Yassahr <yassahr@gmail.com>
2024-07-30 13:59:39 -0400

    new 8kyu solution(combine-names.js)

---
+++ 8kyu/combine-names.js
@@ -0,0 +1,9 @@
+// Create a function named combineNames that accepts two parameters (first and last name). The function should return the full name.
+
+// Example:
+
+// combineNames('James', 'Stevens')
+// returns:
+
+// 'James Stevens'
+let combineNames= (first, last)=> first +' '+ last
