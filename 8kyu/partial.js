279d2e2c59c926a33f63d8de977e61e6cd7108e7
Yassahr <yassahr@gmail.com>
2024-10-30 21:43:19 -0400

    partial attempt

---
+++ 8kyu/partial.js
@@ -0,0 +1,10 @@
+function first(arr, n) {
+    let newArr=[];
+          console.log("boomi")
+    
+      for(let i=0;i<=n;i++){ 
+        console.log("boom")
+        newArr.push(arr[i-1])
+      }
+      return newArr
+    }
