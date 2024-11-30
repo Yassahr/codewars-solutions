191c43342a457bfdcd2f3f02990b725850d1faed
Yassahr <yassahr@gmail.com>
2024-11-30 14:18:56 -0500

    Second solution to billboard-name.js added

--- 8kyu/billboard-name.js
+++ 8kyu/billboard-name.js
@@ -9,3 +9,13 @@
 function billboard(name, price = 30){
     return name.length * price
     } 
+
+//second code option
+function billboard(name, price = 30){
+    let total=0
+    for(let i=1; i<=name.length; i++){
+      total+=price
+      
+    }
+      return total
+    } 
